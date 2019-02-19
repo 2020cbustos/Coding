2bf707d5c2fd89406e45b6a75f8f5278c10f945f
2020cbustos <44789750+2020cbustos@users.noreply.github.com>
2019-02-19 12:18:13 -0500

    Create two.py

---
+++ two.py
@@ -0,0 +1,17 @@
+# task: Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements
+# https://www.codewars.com/kata/54e6533c92449cc251001667/
+
+def unique_in_order(iterable):
+    result = [] # empty list for the result
+    last = None #
+
+    for item in iterable: # going over the array
+        if item == last: # if the value is equal to the previous term
+            continue    #we do nothing
+        else:
+            #if current item is different then previous one
+            result.append(item) # add the item into results
+            last = item #and set previous item to current one
+
+    return result
+
