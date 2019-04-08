2d1b1633378c1bf03e1fe55c0c789f6c0a4061bb
2020cbustos <44789750+2020cbustos@users.noreply.github.com>
2019-04-08 09:16:27 -0400

    Update matrixaddition.py

--- matrixaddition.py
+++ matrixaddition.py
@@ -5,3 +5,11 @@ def matrix_addition(a, b):
     sum = [[a[x][y] + b[x][y] for y in range(len(b))] for x in range(len(a))]
     #sum variable is the addition of the numbers at equal positions for the range of all of them (whole matrix)
     return sum
+
+#IN-CLASS CODE
+def matrix (a,b):
+	for n in range(len(a)):
+		for n in range(len(a[0])):
+			b[n][n] = a[n][n] + b[n][n]
+	return n 
+print(matrix(x,y))
