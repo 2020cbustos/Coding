ac763dcb89dfdea7605849d5bf07c7716bd3439f
2020cbustos <44789750+2020cbustos@users.noreply.github.com>
2019-04-20 16:18:51 -0400

    Update string_increment.py

--- string_increment.py
+++ string_increment.py
@@ -1,3 +1,6 @@
+#the string.strip([remove]) is a built-in python function that removes all the leading and trailing spaces from a string.
+#what goes inside the "remove" is what is taken out, and for this problem, it is the integers from 0 to 9 that are removed 
+
 def increment_string(strng):
     taken = strng.strip('1,2,3,4,5,6,7,8,9,0')#this strips from the string any integers there are
     number = strng[len(taken):]#setting a variable number that will be modified later (number will be the length of the # taken in the string)
@@ -9,4 +12,4 @@ def increment_string(strng):
         new_value = 1 + int(number)#set the new number as 1 more than the integer stripped
         new_value = str(new_value).fill(size)#new value as a string
         return taken + new_value #return the stripped value and new value
-print(increment_string("hey0"))
+#print(increment_string("hey0"))
